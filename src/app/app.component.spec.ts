13cb9fb59aa4970c67040db928a7af70f45be22c
Platypuus <clement8.baille@gmail.com>
2023-03-14 09:52:07 +0100

    app change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -35,34 +35,5 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('h1')?.textContent).toContain(app.title);
   });
 
-  it(`Should have 'dashboard works! in a paragraph`, () => {
-    const fixture = TestBed.createComponent(DashboardComponent);
-    fixture.detectChanges();
-    const app = fixture.componentInstance;
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('p')?.textContent).toContain('dashboard works!');
-  });
 
-  it(`Should have 3 tile rendered if isAdmin is true`, () => {
-    const fixture = TestBed.createComponent(DashboardComponent);
-
-    const app = fixture.componentInstance;
-    app.isAdmin = true;
-
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    console.log(compiled.querySelectorAll('.row')?.length);
-    expect(compiled.querySelectorAll('.tile')?.length).toBe(3);
-  });
-
-  it(`Should have 2 tile rendered if isAdmin is false`, () => {
-    const fixture = TestBed.createComponent(DashboardComponent);
-
-    const app = fixture.componentInstance;
-    app.isAdmin = false;
-
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelectorAll('.tile')?.length).toBe(2);
-  });
 });
