905b1694738bece9acd11a57abeb26a5591f9f11
Platypuus <clement8.baille@gmail.com>
2023-03-14 10:50:06 +0100

    2

--- src/app/student/models/student-model.spec.ts
+++ src/app/student/models/student-model.spec.ts
@@ -4,4 +4,12 @@ describe('StudentModel', () => {
   it('should create an instance', () => {
     expect(new StudentModel()).toBeTruthy();
   });
+  it('should have aubert as lastName an instance', () => {
+    const student:StudentModel= new StudentModel();
+    student.lastName='Aubert';
+    student.email='Aubert@gmail.com';
+    student.login='D4rkAubert';
+    student.password='AubertPsswrd';
+    expect(student.lastName).toBe('Aubert');
+  });
 });
